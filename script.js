660f7dea0280f70fb549ad0cbef67d8006205186
doreenkam <dkamushinda@gmail.com>
2022-04-05 22:42:16 +0100

    Added like button

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
+// ----------- Settings Button ------------
+const settingsMenu = document.querySelector(".settings-menu");
 
-var settingsMenu = document.querySelector(".settings-menu");
-
-
-function settingsMenuToggle(){
-    settingsMenu.classList.toggle("settings-menu-height");
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
 }
 
-// -----------dark mode button------------
+// ----------- Dark Mode Button ------------
 
-var darkBtn = document.getElementById("dark-btn");
+const darkBtn = document.getElementById("dark-btn");
 
-darkBtn.onclick = function(){
-    darkBtn.classList.toggle('dark-btn-on');
-    document.body.classList.toggle("dark-theme");
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
 
-    if(localStorage.getItem("theme") == "light"){
-        localStorage.setItem("theme", "dark");
-    }else{
-        localStorage.setItem("theme", "light");
-    }
-    
-}
-
-if(localStorage.getItem("theme") == "light"){
-
-    darkBtn.classList.remove('dark-btn-on');
-    document.body.classList.remove("dark-theme");
-}
-else if(localStorage.getItem("theme") == "dark"){
-
-    darkBtn.classList.add('dark-btn-on');
-    document.body.classList.add("dark-theme");
-}
-else{
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
     localStorage.setItem("theme", "light");
+  }
+};
+
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
 }
+
+// ----------- Reaction Button Functions ------------
+
+const likeBtn = document.querySelector(".like__btn");
+let likeIcon = document.querySelector("#icon"),
+  count = document.querySelector("#count");
+
+let clicked = false;
+
+likeBtn.addEventListener("click", () => {
+  if (!clicked) {
+    clicked = true;
+    likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    count.textContent++;
+  } else {
+    clicked = false;
+    likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    count.textContent--;
+  }
+});

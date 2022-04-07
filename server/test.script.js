697c0df187e17f8578e50947b74b73ee78495ebc
nay-nae <naomi_emojevbe@hotmail.com>
2022-04-07 10:33:54 +0100

    fixing

---
+++ server/test.script.js
@@ -0,0 +1,11 @@
+// const settingsMenuToggle = require('/settingsMenuToggle')
+
+// test ('menu toggles from dark mode to light mode',() => {
+//     expect(settingsMenuToggle(darkBtn.onclick)).toBe(localStorage.setItem("theme", "dark"))
+// })
+
+const sendApiRequest = require('/sendApiRequest()');
+
+test ('sendApiRequest function exists', () => {
+expect(sendApiRequest()).toBeDefined();
+});
